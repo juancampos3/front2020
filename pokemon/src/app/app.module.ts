d7beef319e4470f89c22628174a9f7a8b5c2b875
jhpulido4 <jhpulido4@poligran.edu.co>
2021-01-14 13:12:25 -0500

    creacion componente header

--- pokemon/src/app/app.module.ts
+++ pokemon/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,

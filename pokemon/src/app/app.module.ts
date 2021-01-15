c1b9ee9a697e3dea527818a1ddadd1178dc28307
anderfront <anricosi@poligran.edu.co>
2021-01-14 19:02:07 -0500

    creacion  de componente footer

--- pokemon/src/app/app.module.ts
+++ pokemon/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
